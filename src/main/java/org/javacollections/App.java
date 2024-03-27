13997f98cb35e1b213ce5656c5778227c8d59734
Kapil Mandloi <kapsmac@Kapils-MacBook-Pro.local>
2024-03-27 10:05:26 +0530

    initial push

---
+++ src/main/java/org/javacollections/App.java
@@ -0,0 +1,13 @@
+package org.javacollections;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "test" );
+    }
+}
